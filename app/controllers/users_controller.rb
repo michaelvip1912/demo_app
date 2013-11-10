de301cdb708e416d42cd8ec267c31b38c26aec43
michael <michaelvip1991@gmail.com>
2013-11-10 21:47:33 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,15 +25,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
@@ -60,6 +57,7 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ActiveRecord::Base
   validates :email, presence: true , format: { with: VALID_EMAIL_REGEX }
   before_save { self.email = email.downcase }
   has_secure_password
-end
+
+
+  def User.new_remember_token
+     SecureRandom.urlsafe_base64
+   end
+
+   def User.encrypt(token)
+     Digest::SHA1.hexdigest(token.to_s)
+   end
+
+   private
+
+     def create_remember_token
+       self.remember_token = User.encrypt(User.new_remember_token)
+     end
+ end
